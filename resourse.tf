c0a033a7b7e3550bd1f280ec13e3d13d00ce646a
SHARUQ <142597507+Sharuqmd@users.noreply.github.com>
2024-05-12 03:55:13 +0530

    Update resourse.tf

--- resourse.tf
+++ resourse.tf
@@ -1,15 +1,12 @@
-# creating vpc and its dependencies 
+# creating vpc and its dependencies
 resource "aws_vpc" "testvpc" {
   cidr_block = "10.0.0.0/16"
-  tags = {
-    "name" = "testvpc"
-  }
 }
 
-resource "aws_subnet" "publicsubnet" { 
-  vpc_id            = aws_vpc.testvpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1"
+resource "aws_subnet" "publicsubnet" {
+  vpc_id                  = aws_vpc.testvpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }
 
@@ -34,12 +31,10 @@ resource "aws_route_table_association" "aws_route_table_association" {
   subnet_id      = aws_subnet.publicsubnet.id
   route_table_id = aws_route_table.routetable.id
 }
-# new resourse for security group creation 
 resource "aws_security_group" "testvpc_aws_security_group" {
   name        = "testvpc_aws_security_group"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.testvpc.id
-
   tags = {
     Name = "testvpc_aws_security_group"
   }
