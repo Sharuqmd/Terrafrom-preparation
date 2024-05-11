18e51960359bd92c429b977cf01c323349613424
SHARUQ <142597507+Sharuqmd@users.noreply.github.com>
2024-05-11 19:36:06 +0530

    Update resourse.tf

--- resourse.tf
+++ resourse.tf
@@ -5,9 +5,31 @@ resource "aws_vpc" "testvpc" {
   }
 }
 
-resource "aws_subnet" "testvpc.publicsubnet" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id = aws_vpc.testvpc.id
+resource "aws_subnet" "publicsubnet" { 
+  vpc_id            = aws_vpc.testvpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-south-1"
   map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "testvpc-internetgateway" {
+  vpc_id = aws_vpc.testvpc.id
+  tags = {
+    "name" = "testvpc-internetgateway"
+  }
+}
+
+resource "aws_route_table" "routetable" {
+  vpc_id = aws_vpc.testvpc.id
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.routetable.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.testvpc-internetgateway.id
+}
+
+resource "aws_route_table_association" "aws_route_table_association" {
+  subnet_id      = aws_subnet.publicsubnet.id
+  route_table_id = aws_route_table.routetable.id
 }
