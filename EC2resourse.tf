cfc8a4b2ce00410eacebd7e4968b155ca402d850
SHARUQ <142597507+Sharuqmd@users.noreply.github.com>
2024-05-11 23:14:42 +0530

    Create EC2resourse.tf

---
+++ EC2resourse.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "testec2" {
+  ami = "ami-05e00961530ae1b55"
+  instance_type = "t2.micro"
+  key_name = terraformkey.pem
+  subnet_id = aws_subnet.publicsubnet.id
+  vpc_security_group_ids = aws_security_group.testvpc_aws_security_group.id
+  user_data = <<-EOF
+  #!/bin/bash
+
+  # Update package lists
+  sudo apt update
+
+  # Install httpd package
+  sudo apt install -y apache2
+
+  # Start the httpd service
+  sudo systemctl start apache2
+
+  # Enable httpd to start on boot
+  sudo systemctl enable apache2
+
+  echo "httpd installation complete!"
+  EOF 
+}
